6429c18e95fd185dc0e8dc8db4e1c31296ae33ea
Stuart Osborne <stuart.osborne@transmode.se>
2016-07-05 13:09:28 +0200

    Prepare for rand.org

--- QuizGen.elm
+++ QuizGen.elm
@@ -2,17 +2,26 @@ module QuizGen exposing (..)
 
 import Models exposing (Multiplication)
 import Random exposing (step, list, initialSeed, pair, int)
+import Http expoisng (..)
 
+
+-- Fallback to homegrown random
 quizGenerator : Int -> Int -> List Multiplication
 quizGenerator size seed =
     let
-       (randomList, _) = step (list size intPairGen) (initialSeed seed)
+       (randomlist, _) = step (list size intPairGen) (initialSeed seed)
 
     in
-       List.indexedMap (\i (a,b) ->  {left = a, right = b, result = 0, index = i}) randomList
+       List.indexedMap (\i (a,b) ->  {left = a, right = b, result = 0, index = i}) randomlist
 
 
 intPairGen : Random.Generator(Int, Int)
 intPairGen =
     pair (int 2 12) (int 2 12)
+
+-- Ask random.org
+-- url: https://api.random.org/json-rpc/1/invoke
+-- api-key: c3a3ec02-bc59-469f-9358-3fe5ac40dd9c
+
+
 
